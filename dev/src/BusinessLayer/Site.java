19e437e9b37f798f1ac9c22386482d40141fce4a
Ido <ids@post.bgu.ac.il>
2022-04-17 22:20:57 +0300

    making classes

---
+++ dev/src/BusinessLayer/Site.java
@@ -0,0 +1,14 @@
+package BusinessLayer;
+
+public class Site {
+    private String contactGuy;
+    private String city;
+    private String phoneNumber;
+    private String street;
+    private String houseNumber;
+    private String floor;
+    private int apartment;
+    private Area area;
+
+
+}
