98e3f62867859ef7f662acc9887327217dc64c82
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-04-19 10:56:35 +0300

    Statred a demo

--- dev/assignment1/PresentationLayer/PresentationController.java
+++ dev/assignment1/PresentationLayer/PresentationController.java
@@ -1,5 +1,13 @@
 package assignment1.PresentationLayer;
 
-public class PresentationController {
+import java.util.Scanner;
 
+public class PresentationController {
+    //** THIS IS A DEMO JUST TO SEE THE PROCESS **//
+    private Scanner sc = new Scanner(System.in);
+    public static void main(String args){
+
+        System.out.println("HI! Welcome to the supplier module, What would you like to do?");
+        System.out.println("1. Create new supplier card\n2. Create new order\n3.");;
+    }
 }
