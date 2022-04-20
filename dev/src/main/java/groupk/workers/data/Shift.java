864f02e3b08d0d8b28f70285c654ea99292692f7
miri volozhinsky <mirivol@post.bgu.ac.il>
2022-04-20 13:47:30 +0300

    Add more Constructors to data

--- dev/src/main/java/groupk/workers/data/Shift.java
+++ dev/src/main/java/groupk/workers/data/Shift.java
@@ -1,16 +1,22 @@
 package groupk.workers.data;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Shift {
     private enum Type {morning, evening};
     private Type type;
     private Date date;
-    private Employee[] staff;
+    private Set<Employee> staff;
 
-    
+    public Shift(String typeString, Date date){
+        type = Type.valueOf(typeString);
+        this.date = date;
+        staff = new HashSet<>();
+    }
     public Type getType() {
         return type;
     }
     public Date getDate() {return date; }
-    public Employee[] getStaff() {return staff; }
+    public Set<Employee> getStaff() {return staff; }
 }
