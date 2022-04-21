e5b9d2cc7876fd9c7102dbe10bb66b543d800498
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-04-21 15:21:00 +0300

    Started with tests

---
+++ dev/assignment1/Tests/ServiceTest.java
@@ -0,0 +1,45 @@
+package assignment1.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void createSupplier() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void editSupplier(){
+        //Create supplier with ppn 11
+        //Edit supplier with ppn 12
+        //Supposed to throw an error saying there isn't a ppn number 12
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteSupplier() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteItem() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createDiscount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteDiscount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteOrder() {
+    }
+}
