acf09b4a8d11ccf499a72c74b3dc6e5d7b69c617
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-05-10 21:10:42 +0300

    Added facade

---
+++ dev/adss_group_k/shared/utils/facade.java
@@ -0,0 +1,13 @@
+package adss_group_k.shared.utils;
+
+public class facade {
+
+    public static void main(String[] args){
+        System.out.println("Welcome to the supplier module!");
+        /**
+         * which module do you need?
+         * 1. supplier module - startSupplierMenu
+         * 2. inventory module -
+         */
+    }
+}
