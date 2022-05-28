b4f723a28a6cedb975c9fe183ef8cd8897ae9283
Yuval Dolev <uvero3@gmail.com>
2022-05-28 15:29:24 +0300

    Implemented ItemInOrderDAO

--- dev/adss_group_k/dataLayer/dao/ItemInOrderDAO.java
+++ dev/adss_group_k/dataLayer/dao/ItemInOrderDAO.java
@@ -3,7 +3,10 @@ package adss_group_k.dataLayer.dao;
 import adss_group_k.dataLayer.records.ItemInOrderRecord;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
@@ -13,44 +16,38 @@ public class ItemInOrderDAO extends BaseDAO<ItemInOrderRecord.ItemInOrderKey, It
         super(conn);
     }
 
-    @Override
-    ItemInOrderRecord fetch(ItemInOrderRecord.ItemInOrderKey key) throws SQLException, NoSuchElementException {
-        return null;
-    }
 
     @Override
     Stream<ItemInOrderRecord> fetchAll() throws SQLException {
-        return null;
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM ItemInOrder");
+        ResultSet query = ps.executeQuery();
+        ArrayList<ItemInOrderRecord> list = new ArrayList<>();
+        while (query.next()) {
+            ItemInOrderRecord record = new ItemInOrderRecord(
+                    query.getInt("itemSupplierPPN"),
+                    query.getInt("itemCatalogNumber"),
+                    query.getInt("orderId"),
+                    query.getInt("qty")
+            );
+            list.add(record);
+        }
+        return list.stream();
     }
 
     @Override
-    protected int runDeleteQuery(ItemInOrderRecord.ItemInOrderKey itemInOrderKey) {
-        return 0;
-    }
-
-    public void updateAmount(int orderId, int ppn, int catalogNumber, int amount) {
-
-    }
-
-    public ItemInOrderRecord getOrCreate(int orderId, int ppn, int catalogNumber) {
-        ItemInOrderRecord.ItemInOrderKey key = new ItemInOrderRecord.ItemInOrderKey(ppn, catalogNumber, orderId);
-        if(exists(key)) {
-            return get(key).data;
+    ItemInOrderRecord fetch(ItemInOrderRecord.ItemInOrderKey key) throws SQLException, NoSuchElementException {
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM ItemInOrder " +
+                "WHERE itemSupplierPPN=? AND itemCatalogNumber=? AND orderId=?");
+        ps.setInt(1, key.ppn);
+        ps.setInt(2, key.catalogNumber);
+        ps.setInt(3, key.orderId);
+        ResultSet query = ps.executeQuery();
+        if (query.next()) {
+            ItemInOrderRecord record = new ItemInOrderRecord(key.ppn,
+                    key.catalogNumber, key.orderId, query.getInt("qty"));
+            return record;
         }
-        return create(ppn, catalogNumber, orderId);
-    }
-
-    private ItemInOrderRecord create(int ppn, int catalogNumber, int orderId) {
-        return create(
-                () -> new ItemInOrderRecord(ppn, catalogNumber, orderId, 0),
-
-                 "INSERT INTO ItemInOrder(qty, itemSupplierPPN, itemCatalogNumber," +
-                "orderId) VALUES(0, ?, ?, ?)",
-
-                ps -> ps.setInt(1, ppn),
-                ps -> ps.setInt(2, catalogNumber),
-                ps -> ps.setInt(3, orderId)
-        ).getOrThrow(RuntimeException::new);
+        throw new NoSuchElementException();
     }
 
     /*
@@ -61,4 +58,47 @@ public class ItemInOrderDAO extends BaseDAO<ItemInOrderRecord.ItemInOrderKey, It
 	"orderId"	INTEGER NOT NULL,
 	PRIMARY KEY("itemSupplierPPN","itemCatalogNumber","orderId")
      */
+
+    @Override
+    protected int runDeleteQuery(ItemInOrderRecord.ItemInOrderKey key) {
+        return runUpdate("DELETE FROM ItemInOrder WHERE itemSupplierPPN=? " +
+                        "AND itemCatalogNumber=? AND orderId=?",
+                ps -> ps.setInt(1, key.ppn),
+                ps -> ps.setInt(2, key.catalogNumber),
+                ps -> ps.setInt(3, key.orderId));
+    }
+
+    public void updateAmount(int orderId, int ppn, int catalogNumber, int amount) {
+        ItemInOrderRecord record = getOrCreate(orderId, ppn, catalogNumber);
+        record.setAmount(amount);
+        runUpdate("UPDATE ItemInOrder SET qty=?" +
+                        "WHERE itemSupplierPPN=? " +
+                        " AND itemCatalogNumber=? AND orderId=?",
+                ps -> ps.setInt(1, amount),
+                ps -> ps.setInt(2, ppn),
+                ps -> ps.setInt(3, catalogNumber),
+                ps -> ps.setInt(4, amount)
+        );
+    }
+
+    public ItemInOrderRecord getOrCreate(int orderId, int ppn, int catalogNumber) {
+        ItemInOrderRecord.ItemInOrderKey key = new ItemInOrderRecord.ItemInOrderKey(ppn, catalogNumber, orderId);
+        if (exists(key)) {
+            return get(key).data;
+        }
+        return create(ppn, catalogNumber, orderId);
+    }
+
+    private ItemInOrderRecord create(int ppn, int catalogNumber, int orderId) {
+        return create(
+                () -> new ItemInOrderRecord(ppn, catalogNumber, orderId, 0),
+
+                "INSERT INTO ItemInOrder(qty, itemSupplierPPN, itemCatalogNumber," +
+                        "orderId) VALUES(0, ?, ?, ?)",
+
+                ps -> ps.setInt(1, ppn),
+                ps -> ps.setInt(2, catalogNumber),
+                ps -> ps.setInt(3, orderId)
+        ).getOrThrow(RuntimeException::new);
+    }
 }
