b02ba0dff99769d407634cf4460850685d9a9bfd
Batel Shkolnik <batelshk@post.bgu.ac.il>
2022-06-13 20:56:41 +0300

    Fixed warning for deleteing supplier

--- dev/adss_group_k/BusinessLayer/Suppliers/Controller/ItemController.java
+++ dev/adss_group_k/BusinessLayer/Suppliers/Controller/ItemController.java
@@ -5,6 +5,7 @@ import adss_group_k.BusinessLayer.Suppliers.BusinessLogicException;
 import adss_group_k.BusinessLayer.Suppliers.BussinessObject.Item;
 import adss_group_k.BusinessLayer.Suppliers.BussinessObject.QuantityDiscount;
 import adss_group_k.BusinessLayer.Suppliers.BussinessObject.Supplier;
+import adss_group_k.PresentationLayer.Suppliers.UserOutput;
 import adss_group_k.dataLayer.dao.PersistenceController;
 import adss_group_k.dataLayer.records.ItemRecord;
 import adss_group_k.dataLayer.records.readonly.ItemData;
@@ -79,7 +80,12 @@ public class ItemController {
 
     public void deleteAllFromSupplier(Supplier s) {
         for(Map.Entry<String, Item> entry: items.entrySet()) {
-            items.remove(entry.getKey());
+            String key = entry.getKey();
+            Item item = entry.getValue();
+            items.remove(key);
+            dal.getItems().delete(new ItemRecord.ItemKey(item.getSupplier().getPpn(), item.getCatalogNumber()));
+            UserOutput.getInstance().println("Item with catalog number: " +entry.getValue().getCatalogNumber() +
+                    " is deleted.");
         }
     }
 
