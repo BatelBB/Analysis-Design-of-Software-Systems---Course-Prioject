dd9fa5c50e381d765adc3f4a8347da75b387ff85
Yuval Dolev <uvero3@gmail.com>
2022-06-15 13:04:24 +0300

    Small fix: NOW it compiles

--- dev/src/main/java/groupk/Main.java
+++ dev/src/main/java/groupk/Main.java
@@ -1,8 +1,6 @@
 package groupk;
 
 import groupk.shared.PresentationLayer.App;
-import groupk.shared.PresentationLayer.MainInventoryAndSuppliers;
-
 public class Main {
     public static void main(String[] args) {System.out.println("Welcome!");
         App app = new App("database.db");
