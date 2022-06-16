776170cd781bcaa2fe16f751340118ec93c9488e
Yuval Dolev <uvero3@gmail.com>
2022-06-16 15:22:37 +0300

    removed a usage of useService when not needed

--- dev/src/main/java/groupk/shared/PresentationLayer/Inventory/InventoryPresentationFacade.java
+++ dev/src/main/java/groupk/shared/PresentationLayer/Inventory/InventoryPresentationFacade.java
@@ -325,7 +325,7 @@ public class InventoryPresentationFacade {
         System.out.println("please enter actual amount delivered\n(example format: \"[id0]-[amount0]_[id1]-[amount1]\"):");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        useService(args, 1, () -> facade.confirmOrderAmount(convertMap(input)));
+        facade.confirmOrderAmount(convertMap(input));
     }
 
     //converters
