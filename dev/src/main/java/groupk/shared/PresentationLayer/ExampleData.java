5001e757b8267ae879eef15e8c031062ced234ab
Yuval Dolev <uvero3@gmail.com>
2022-06-16 14:36:53 +0300

    Fixed example data formats

--- dev/src/main/java/groupk/shared/PresentationLayer/ExampleData.java
+++ dev/src/main/java/groupk/shared/PresentationLayer/ExampleData.java
@@ -12,16 +12,18 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class ExampleData {
-    public static void loadExampleData(InventoryService inventory, ISupplierService suppliers) {
+    public static void loadExampleData(InventoryService inventory, ISupplierService suppliers
+        // TODO logistics & employees - add your stuff here
+    ) {
         int ppn_office = suppliers.createSupplier(1, 1, "OfficeLi",
                 true, PaymentCondition.Credit, DayOfWeek.SUNDAY,
-                "Ofir", "1-800-office", "Pencil 12, Pencilvannia").data.getPpn();
+                "Ofir", "054-1234567", "Pencil 12, Pencilvannia").data.getPpn();
         int ppn_foods = suppliers.createSupplier(2, 2, "FoodsAreUs",
                 true, PaymentCondition.DirectDebit, DayOfWeek.THURSDAY,
-                "Frank", "1-800-food", "Vegetable Street, Beersheba").data.getPpn();
+                "Frank", "050-1234569", "Vegetable 12, Beersheba").data.getPpn();
         int ppn_lorem = suppliers.createSupplier(3, 3, "Lorem Stuff",
                 true, PaymentCondition.Credit, DayOfWeek.WEDNESDAY,
-                "Lauren", "1-800-Lorem", "Lorem, Ipsumia").data.getPpn();
+                "Lauren", "052-7654321", "Lorem 14, Ipsumia").data.getPpn();
 
         inventory.addCategory("office");
         inventory.addSubCategory("office", "writing");
@@ -192,5 +194,7 @@ public class ExampleData {
         // fill other tables
         inventory.createByCategoryReport("my first report", "Naziff", "", "", "");
         inventory.createBySupplierReport("my second report", "Shariff", 2);
+
+        // TODO employees, logistics, add your things here
     }
 }
